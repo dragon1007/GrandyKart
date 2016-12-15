315e0ac4ba89d587fbd325b0b65f202f5307c244
Kevin Kasson <kkasson@ca.thomasaquinas.edu>
2016-12-15 12:38:42 -0800

    changed xml from xDoc to jQuery

--- public_html/js/gk.js
+++ public_html/js/gk.js
@@ -41,12 +41,10 @@ function updateRacers() {
         var maxRacers = 16;
         var players = [];
         for (i = 0; i < keysData.length; i++) {
-            var ri = racers.id.indexOf(keysData[i].racer);
-            var pi = prestige.level.indexOf(keysData[i].prestige);
-            var rn = racers[ri].name;
-            var rm = racers[ri].image;
-            var pn = prestige[pi].name;
-            var pm = prestige[pi].image;
+            var rn = racers[keysData[i].racer].name;
+            var rm = racers[keysData[i].racer].image;
+            var pn = prestige[keysData[i].prestige].name;
+            var pm = prestige[keysData[i].prestige].image;
             players.push({
                 "name": keysData[i].name,
                 "racer": {
@@ -89,7 +87,7 @@ function updateRacers() {
             if (i % maxRacers == 0) {
                 d.append('<div style="float: left; width: 210px; display: none;">');
             }
-            d.children().last().append('<div><img src="' + player.prestige.image + '" alt="' + player.prestige.name + '" /> ' + player.name + " - " + player.keys + '<img style="height: 32px; width: 32px;" src="' + player.racer.image + '" alt="' + player.racer.name + '" /></div>');
+            d.children().last().append('<div><img style="height: 32px; width: 32px;" src="' + player.prestige.image + '" alt="' + player.prestige.name + '" /> ' + player.name + " - " + player.keys + '<img style="height: 32px; width: 32px;" src="' + player.racer.image + '" alt="' + player.racer.name + '" /></div>');
         });
         if (d.children().length > 0) {
             $(d.children()[0]).fadeIn();
@@ -105,65 +103,33 @@ $(document).ready(function () {
     document.t = setInterval(update, 5000);
 });
 
-function getPrestigeImage(p) {
-    switch (p) {
-        case 0:
-            return "null.png";
-            break;
-        case 1:
-            return "mushroom.png";
-            break;
-        case 2:
-            return "fireFlower.png";
-            break;
-        case 3:
-            return "leaf.png";
-            break;
-        case 4:
-            return "capeFeather.png";
-            break;
-    }
-}
 function loadConfig() {
-    if (document.implementation && document.implementation.createDocument) {
-        // this is the W3C DOM way, supported so far only in NN6+
-        xDoc = document.implementation.createDocument("", "theXdoc", null);
-    } else if (typeof ActiveXObject !== "undefined") {
-        // make sure real object is supported (sorry, IE5/Mac)
-        if (document.getElementById("msxml").async) {
-            xDoc = new ActiveXObject("Msxml.DOMDocument");
-        }
-    }
-    xDoc.load("resources/config.xml");
-
-    setTimeout(contLoad, 1000);
-}
-function contLoad() {
-    jsonFile = xDoc.getElementsByTagName("json")[0].firstChild.nodeValue;
+    $.get("resources/config.xml", function(xmlData) {
+    $xml = $($.parseXML(xmlData));
+    
+    jsonFile = $xml.find('json')[0].textContent;
 
     racers = [];
     prestige = [];
 
-    var racerTags = xDoc.getElementsByTagName("racers");
-    for (var i = 0; i < racerTags.length; i++) {
-        var id = racerTags[i].getElementsByTagName("id")[0].firstChild.nodeValue;
-        var name = racerTags[i].getElementsByTagName("displayName")[0].firstChild.nodeValue;
-        var image = racerTags[i].getElementsByTagName("image")[0].firstChild.nodeValue;
-        racers.push({
-            "id": id,
+    $xml.find('racers').each(function() {
+      var id = $(this).find('id')[0].textContent;
+      var name = $(this).find('displayName')[0].textContent;
+      var image = $(this).find('image')[0].textContent;
+      racers[id] = {
+          "name": name,
+          "image": image
+      };
+    });
+
+    $xml.find('prestige').each(function() {
+      var level = $(this).find('level')[0].textContent;
+      var name = $(this).find('displayName')[0].textContent;
+      var image = $(this).find('image')[0].textContent;
+        prestige[level] = {
             "name": name,
             "image": image
-        });
-    }
-    var prestigeTags = xDoc.getElementsByTagName("prestige");
-    for (var i = 0; i < prestigeTags.length; i++) {
-        var level = prestigeTags[i].getElementsByTagName("level")[0].firstChild.nodeValue;
-        var name = prestigeTags[i].getElementsByTagName("displayName")[0].firstChild.nodeValue;
-        var image = prestigeTags[i].getElementsByTagName("image")[0].firstChild.nodeValue;
-        prestige.push({
-            "level": level,
-            "name": name,
-            "image": image
-        });
-    }
+        };
+    });
+  });
 }
